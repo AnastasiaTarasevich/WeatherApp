18c674f42b14034e3023096add6498db188c3dd3
AnastasiaTarasevich <tarasevich-nastya@bk.ru>
2025-09-23 18:14:12 +0300

    refactor: change location position

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,2 @@
 export const OPENWEATHER_API =
   'https://api.openweathermap.org/data/2.5/weather';
-export const IP_API = 'https://ipapi.co/json/';
--- src/store/sagas/geoPosition.ts
+++ src/store/sagas/geoPosition.ts
@@ -1,4 +1,4 @@
-import { IP_API, OPENWEATHER_API } from '@constants/api';
+import { OPENWEATHER_API } from '@constants/api';
 import { FETCH_LOCATION_REQUEST } from '@constants/locationFetch';
 import {
   fetchLocationFailure,
@@ -8,21 +8,29 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 
 const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
+function getCurrentPosition() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
 function* fetchLocationSaga() {
   try {
-    const ipRes = yield call(fetch, IP_API);
-    const ipData = yield call([ipRes, ipRes.json]);
-    const coords = { lat: ipData.latitude, lon: ipData.longitude };
+    const position= yield call(getCurrentPosition);
+    const coords = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    };
 
-    const res = yield call(
+    const res: Response = yield call(
       fetch,
       `${OPENWEATHER_API}?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=metric&lang=ru`
     );
     const data = yield call([res, res.json]);
 
     yield put(fetchLocationSuccess(data.name, coords.lat, coords.lon));
-  } catch {
-    yield put(fetchLocationFailure('Error for geo'));
+  } catch (err) {
+    yield put(fetchLocationFailure(err.message));
   }
 }
 
